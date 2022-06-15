bae019fde94c192c4637aee6541394f58141c640
Daisuke Osanai <dsk.osanai@gmail.com>
2022-06-15 16:25:41 +0900

    stream on

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // _actionStream(context);
+    _actionStream(context);
     // createScaffoldMessengerStreamListen(context);
 
     return MaterialApp(
