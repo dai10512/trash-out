6ecb0939f33584a495afc31f89731aa9c98831e4
Daisuke Osanai <dsk.osanai@gmail.com>
2022-06-15 15:47:34 +0900

    version up

--- lib/typeAdapter/trashOfDay.g.dart
+++ lib/typeAdapter/trashOfDay.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'TrashOfDay.dart';
+part of 'trashOfDay.dart';
 
 // **************************************************************************
 // TypeAdapterGenerator
